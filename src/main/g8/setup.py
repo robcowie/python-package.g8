8f190d7bb978d6df2ba8411e323187287ed8b4aa
Rob Cowie <rob_cowie@mac.com>
2012-09-28 13:12:00 +0100

    More testing

--- src/main/g8/setup.py
+++ src/main/g8/setup.py
@@ -1,18 +1,18 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='$pacakge_name;format="lower,snake"$',
+    name='$package_name;format="lower,snake"$',
     version='$version$',
     author='$author$',
     author_email='$author_email$',
     packages=find_packages(),
     description='$description$',
-    test_suite='$pacakge_name$.tests.get_suite',
+    test_suite='$package_name$.tests.get_suite',
     include_package_data=True,
     zip_safe=False,
     entry_points = {
         'console_scripts': [
-            '$pacakge_name;format="lower,snake"$ = $name$.__main__:main'
+            '$package_name;format="lower,snake"$ = $name$.__main__:main'
         ]
     },
     install_requires=[]
