9c32cc57a3fbc7911dd2a5fed9da8bbad436a04e
Rob Cowie <rob_cowie@mac.com>
2012-09-28 13:11:00 +0100

    More testing

--- src/main/g8/setup.py
+++ src/main/g8/setup.py
@@ -1,18 +1,18 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='$packge_name;format="lower,snake"$',
+    name='$pacakge_name;format="lower,snake"$',
     version='$version$',
     author='$author$',
     author_email='$author_email$',
     packages=find_packages(),
     description='$description$',
-    test_suite='$name$.tests.get_suite',
+    test_suite='$pacakge_name$.tests.get_suite',
     include_package_data=True,
     zip_safe=False,
     entry_points = {
         'console_scripts': [
-            'pamhdb = $name$.__main__:main'
+            '$pacakge_name;format="lower,snake"$ = $name$.__main__:main'
         ]
     },
     install_requires=[]
